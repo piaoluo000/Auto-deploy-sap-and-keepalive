106a5100eca8d29cdc84b4cd7c845508d004c436
piaoluo000 <quanliu666@126.com>
2025-10-16 00:59:42 +0800

    Update keep.sh

--- keep.sh
+++ keep.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # 环境变量配置
-EMAIL="xxxxx@xxxx.com"   # 登录邮箱
-PASSWORD="xxxxxxxxx"     # 登录密码
+EMAIL="zyqhk@tiffincrane.com"   # 登录邮箱
+PASSWORD="Bjl8831484"     # 登录密码
 
 # 要保活的URL列表,多个用英文空格分隔
-URLS="https://xxxx.cfapps.ap21.hana.ondemand.com https://xxxx.cfapps.us10-001.hana.ondemand.com"  
+URLS="https://free-sgtifonb.cfapps.ap21.hana.ondemand.com https://free-uskriebg.cfapps.us10-001.hana.ondemand.com"  
 
 # 颜色定义
 green() {
