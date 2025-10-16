8bff560808f05240d7adc28653293d6ea4d0352d
piaoluo000 <quanliu666@126.com>
2025-10-16 10:10:43 +0800

    Update _worker-keep.js

--- _worker-keep.js
+++ _worker-keep.js
@@ -1,6 +1,6 @@
 // 环境变量配置(必填)
-let email = "你的sap登录邮箱";      // SAP登录邮箱,直接填写或设置环境变量，变量名：EMAIL
-let password = "你的sap登录密码";   // SAP登录密码,直接填写或设置环境变量，变量名：PASSWORD
+let email = "zyqhk@tiffincrane.com";      // SAP登录邮箱,直接填写或设置环境变量，变量名：EMAIL
+let password = "Bjl8831484";   // SAP登录密码,直接填写或设置环境变量，变量名：PASSWORD
 
 // 离线重启通知 Telegram配置(可选)
 let CHAT_ID = "";    // Telegram聊天CHAT_ID,直接填写或设置环境变量，变量名：CHAT_ID
@@ -8,7 +8,7 @@ let BOT_TOKEN = "";  // Telegram机器人TOKEN,直接填写或设置环境变量
 
 // 应用配置 URL和应用名称配置(必填)
 const MONITORED_APPS = [ // 格式: {url: "应用URL", name: "应用名称"}
-  {url: "https://xxxxx.cfapps.ap21.hana.ondemand.com", name: "xxxxx"},  
+  {url: "https://USSAP.cfapps.us10-001.hana.ondemand.com", name: "USSAP"},  
   {url: "https://xxxxx.cfapps.us10-001.hana.ondemand.com", name: "xxxxx"}
 ];
 
